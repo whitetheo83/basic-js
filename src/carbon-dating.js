206bd97bcf0a1992b736343bd82e568976b71868
Evgeni Bochkov <60480912+whitetheo83@users.noreply.github.com>
2021-09-21 18:47:51 +0400

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,14 +17,33 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(data) {
-    var x = NaN;
-    data = Number(data);
-     if (Number.isNaN(data)) {
-        return (false);
-    } 
-
-    return (Math.ceil(Math.log(15/data)/(0.693/5730)));
-
-  // remove line with error and write your code here
-}
+/* export default function dateSample(data) {
+  data = Number(data);
+  if (data.toFixed){
+    if (data > 0) {
+      let x = Math.ceil(Math.log(15/data)/(0.693/5730));
+      if ( x > 0) {
+        return x;
+      }
+    }
+  } */
+  export default function dateSample(data) {
+    if (!data) {
+      return false;
+    } else if (data[0] == undefined) {
+      return false;
+    }
+    else if (typeof(data) == 'string') {
+    if (String(parseFloat(data)).length == data.length) {
+      if (parseFloat(data) > 0 && (parseFloat(data) < 15)) {
+        let x = Math.ceil(Math.log(15/data)/(0.693/5730));
+        if ( x > 0) {
+          return x;
+        }
+      }
+    } else {
+      return false;
+    }
+  }
+  return false;
+  }
